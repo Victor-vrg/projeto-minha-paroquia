f2bfd314b8aad4cf76e515172f1da60d656a7542
Victor-vrg <victorvrg2003@gmail.com>
2023-10-13 11:41:48 -0300

    estilização do header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,34 +1,27 @@
-import React, { MouseEvent, useState, useEffect } from 'react';
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, styled } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import NavigationRail from './NavigationRail';
-import '@fontsource/roboto/400.css';
-
-const MyAppBar = styled(AppBar)(({ theme }) => ({
-  top: 0,
-  zIndex: 2,
-  backgroundColor: '#fff',
-  boxShadow: 'none', 
-  marginBottom: 10,
-}));
-
-const MyTypography = styled(Typography)(({ theme }) => ({
-  fontSize: '24px',
-  lineHeight: '1.2',
-  color: '#333',
-  padding: '10px',
-  textAlign: 'center',
-  flexGrow: 1,
-}));
-
-const MyIconButton = styled(IconButton)(({ theme }) => ({
-  color: '#333',
-  marginRight: '20px',
-  cursor: 'pointer',
-}));
+import React, { MouseEvent, useState, useEffect } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import EventIcon from "@mui/icons-material/Event";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
+import "@fontsource/roboto/400.css";
+import '../styles/header.css';
 
 const Header: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleMenuOpen = (event: MouseEvent<HTMLButtonElement>) => {
@@ -47,47 +40,74 @@ const Header: React.FC = () => {
     // Implemente o redirecionamento para a página de edição de perfil (futuro)
   };
 
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <MyAppBar className='header-bar'>
-      <Toolbar>
-        {windowWidth >= 601 ? <NavigationRail /> : null}
-        <MyTypography variant="h6">
-          Nome da Paróquia
-        </MyTypography>
-        <MyIconButton
-          onClick={handleMenuOpen}
-          edge="end"
-          color="inherit"
-          aria-label="menu do usuário"
-        >
-          <AccountCircleIcon />
-        </MyIconButton>
-        <Menu
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={handleMenuClose}
-        >
-          <MenuItem onClick={handleEditProfile}>
-            Editar Perfil
-          </MenuItem>
-          <MenuItem onClick={handleLogout}>
-            Sair
-          </MenuItem>
-        </Menu>
-      </Toolbar>
-    </MyAppBar>
+    <div className="header">
+      <AppBar className="header-bar">
+        <Toolbar className="toolbar">
+          <IconButton
+            onClick={toggleDrawer}
+            edge="start"
+            color="inherit"
+            aria-label="menu de navegação"
+            className="botão-de-navegação"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className="nome-paroquia" variant="h6">
+            Nome da Paróquia
+          </Typography>
+          <IconButton
+            onClick={handleMenuOpen}
+            edge="end"
+            color="inherit"
+            aria-label="menu do usuário"
+            className="botão-de-perfil"
+          >
+            <AccountCircleIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+          >
+            <MenuItem onClick={handleEditProfile}>Editar Perfil</MenuItem>
+            <MenuItem onClick={handleLogout}>Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer}>
+        <List>
+          <ListItem button onClick={toggleDrawer}>
+            <ListItemIcon>
+              <EventIcon />
+            </ListItemIcon>
+            <ListItemText primary="Eventos" />
+          </ListItem>
+          <ListItem button onClick={toggleDrawer}>
+            <ListItemIcon>
+              <FlightTakeoffIcon />
+            </ListItemIcon>
+            <ListItemText primary="Excursões" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
   );
 };
 
